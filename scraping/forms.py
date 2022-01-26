f5a646b98143a7691f1f923f5c38f96ab1a17d8e
Buzhak <byzhak@ya.ru>
2022-01-26 20:22:19 +0300

    add soup

--- scraping/forms.py
+++ scraping/forms.py
@@ -3,6 +3,7 @@ from .models import City, Language
 
 
 class FindForm(forms.Form):
+    
     city = forms.ModelChoiceField(
         queryset=City.objects.all(), 
         to_field_name="slug", 
@@ -10,11 +11,12 @@ class FindForm(forms.Form):
         widget=forms.Select(attrs={'class':'form-control'}), 
         label='Город'
     )
+
     language = forms.ModelChoiceField(
         queryset=Language.objects.all(), 
         to_field_name="slug", 
         required=False, 
         widget=forms.Select(attrs={'class':'form-control'}), 
-        label = 'Язык прогромирования'
+        label = 'Специальность'
     )
     
